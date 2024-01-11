f9cae5bcc8b67e588b6c82f06c3aa60b172c20df
alexraduca01 <alex.raduca01@icloud.com>
2024-01-11 17:54:12 +0100

    fixed characters index style

--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -6,23 +6,23 @@
             <div class="container">
                 <div class="row gy-4 gap-2">
                     @foreach ($characters as $item)
-                    <div class="card border-0 p-0" style="width: calc(100% / 5 - 0.5rem);">
+                    <div class="card border-0 rounded p-0" style="width: calc(100% / 5 - 0.5rem); background-color: rgba(13, 13, 34, 0.733);">
                         <img src="https://picsum.photos/250/250" class="card-img-top" alt="{{ $item->name }}">
-                        <div class="card-body text-center bg-dark text-white">
+                        <div class="card-body text-center text-white b-0" style="">
                             <h5 class="card-title">{{ $item->name }}</h5>
                             <p>
-                                <span><span style="color: red">Attack:</span> {{ $item->attack }}</span>
-                                <span><span style="color: blue">Defense:</span> {{ $item->defence }}</span>
+                                <span><span style="color: red">Atk:</span> {{ $item->attack }}</span>
+                                <span><span style="color: blue">Def:</span> {{ $item->defence }}</span>
                             </p>
                             <p>
-                                <span><span style="color: green">Life:</span> {{ $item->life }}</span>
+                                <span><span style="color: green">HP:</span> {{ $item->life }}</span>
                                 <span><span style="color: yellow">Speed:</span> {{ $item->speed }}</span>
                             </p>
+                            <a class="btn btn-primary" href="{{ route('characters.show', $item) }}">Vedi Dettagli</a>
                         </div>
                       </div>
                     @endforeach
                 </div>
-
             </div>
         </div>
     </section>
