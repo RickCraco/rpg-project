b36a918860245638dbda03018085f62869f96713
alexraduca01 <alex.raduca01@icloud.com>
2024-01-14 12:44:20 +0100

    changed characters/index style

--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -6,24 +6,116 @@
             <div class="container">
                 <div class="row gy-4 gap-2">
                     @foreach ($characters as $item)
-                    <div class="card border-0 rounded p-0" style="width: calc(100% / 5 - 0.5rem); background-color: rgba(13, 13, 34, 0.733);">
-                        <img src="https://picsum.photos/250/250" class="card-img-top" alt="{{ $item->name }}">
-                        <div class="card-body text-center text-white b-0" style="">
-                            <h5 class="card-title">{{ $item->name }}</h5>
-                            <p>
-                                <span><span style="color: red">Atk:</span> {{ $item->attack }}</span>
-                                <span><span style="color: blue">Def:</span> {{ $item->defence }}</span>
-                            </p>
-                            <p>
-                                <span><span style="color: green">HP:</span> {{ $item->life }}</span>
-                                <span><span style="color: yellow">Speed:</span> {{ $item->speed }}</span>
-                            </p>
-                            <a class="btn btn-primary" href="{{ route('characters.show', $item) }}">Vedi Dettagli</a>
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front d-flex flex-column justify-content-end" style="background-image: url('https://picsum.photos/300/300');">
+                                <h3 class="card-title p-2 text-center text-white" style="background-color: rgba(0, 0, 0, 0.5);">{{ $item->name }}</h3>
+                            </div>
+                            <div class="flip-card-back">
+                                <div class="text-white">
+                                    <div class="px-2 d-flex flex-column">
+                                        <div class="fs-3">
+                                            <span style="color: red">Atk</span>
+                                            <div class="d-flex align-items-center justify-content-between ">
+                                                <div class="d-flex">
+                                                    <div class="left-line" style="width:
+                                                        @php
+                                                            $percentage = $item->attack / 20 * 100;
+                                                            $percentage = $percentage * 3;
+                                                            echo $percentage;
+                                                        @endphp\px; background-color: red;">
+                                                    </div>
+                                                    <div class="right-line"
+                                                        style="width: @php
+                                                                $difference = 20 - $item->attack;
+                                                                $percentage = $difference / 20 * 100;
+                                                                $percentage = $percentage * 3;
+                                                                echo $percentage;
+                                                            @endphp\px;">
+                                                    </div>
+                                                </div>
+                                                <span style="color: red">{{ $item->attack }}</span>
+                                            </div>
+                                        </div>
+                                        <div class="fs-3">
+                                            <span style="color: blue">Def</span>
+                                            <div class="d-flex align-items-center justify-content-between ">
+                                                <div class="d-flex">
+                                                    <div class="left-line" style="width:
+                                                        @php
+                                                            $percentage = $item->defence / 20 * 100;
+                                                            $percentage = $percentage * 3;
+                                                            echo $percentage;
+                                                        @endphp\px; background-color: blue;">
+                                                    </div>
+                                                    <div class="right-line"
+                                                        style="width: @php
+                                                                $difference = 20 - $item->defence;
+                                                                $percentage = $difference / 20 * 100;
+                                                                $percentage = $percentage * 3;
+                                                                echo $percentage;
+                                                            @endphp\px;">
+                                                    </div>
+                                                </div>
+                                                <span style="color: blue">{{ $item->defence }}</span>
+                                            </div>
+                                        </div>
+                                        <div class="fs-3">
+                                            <span style="color: green">HP</span>
+                                            <div class="d-flex align-items-center justify-content-between ">
+                                                <div class="d-flex">
+                                                    <div class="left-line" style="width:
+                                                        @php
+                                                            $percentage = $item->life / 100 * 100;
+                                                            $percentage = $percentage * 3;
+                                                            echo $percentage;
+                                                        @endphp\px; background-color: green;">
+                                                    </div>
+                                                    <div class="right-line"
+                                                        style="width: @php
+                                                                $difference = 100 - $item->life;
+                                                                $percentage = $difference / 100 * 100;
+                                                                $percentage = $percentage * 3;
+                                                                echo $percentage;
+                                                            @endphp\px;">
+                                                    </div>
+                                                </div>
+                                                <span style="color: green">{{ $item->life }}</span>
+                                            </div>
+                                        </div>
+                                        <div class="fs-3">
+                                            <span style="color: yellow">Spd</span>
+                                            <div class="d-flex align-items-center justify-content-between ">
+                                                <div class="d-flex">
+                                                    <div class="left-line" style="width:
+                                                        @php
+                                                            $percentage = $item->speed / 20 * 100;
+                                                            $percentage = $percentage * 3;
+                                                            echo $percentage;
+                                                        @endphp\px; background-color: yellow;">
+                                                    </div>
+                                                    <div class="right-line"
+                                                        style="width: @php
+                                                                $difference = 20 - $item->speed;
+                                                                $percentage = $difference / 20 * 100;
+                                                                $percentage = $percentage * 3;
+                                                                echo $percentage;
+                                                            @endphp\px;">
+                                                    </div>
+                                                </div>
+                                                <span style="color: yellow">{{ $item->speed }}</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a class="btn btn-primary fs-3" href="{{ route('characters.show', $item) }}" style="position: absolute; bottom: 20px; left: 50%; transform: translateX(-50%); ">Vedi Dettagli</a>
+                                </div>
+                            </div>
                         </div>
-                      </div>
+                    </div>
                     @endforeach
                 </div>
             </div>
         </div>
     </section>
 @endsection
+
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,17 @@
     <section class="container">
         <h1>Section title</h1>
         <p>section content</p>
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
+                </div>
+                <div class="flip-card-back">
+                    <h1>John Doe</h1>
+                    <p>Architect & Engineer</p>
+                    <p>We love that guy</p>
+                </div>
+            </div>
+        </div>
     </section>
 @endsection
