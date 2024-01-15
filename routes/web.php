9aa2707d0a0cf3040bfe0e9fd68f2b9203884abb
alexraduca01 <alex.raduca01@icloud.com>
2024-01-15 16:18:31 +0100

    fix

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,8 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('characters', CharacterController::class);
 });
-
-Route::resource('characters', CharacterController::class);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
