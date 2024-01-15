195505f4b8b13e20c3aaf5fda4104bc1fc6b942c
alexraduca01 <alex.raduca01@icloud.com>
2024-01-15 18:22:06 +0100

    fixed edit

--- resources/views/admin/characters/edit.blade.php
+++ resources/views/admin/characters/edit.blade.php
@@ -2,49 +2,25 @@
 @section('content')
 
 <div class="container">
-        @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-        @endif
-        <form action="{{route('character.update', $character->id)}}" method="POST" class="p-3">
-            @csrf
-            @method('PUT')
-                <input type="text" id="name" value="{{old('name', $character->name)}}" name="name" placeholder="Name" class="form-control my-2" required>
-                <input type="text" id="description" value="{{old('description', $character->description)}}" name="Description" placeholder="Description" class="form-control my-2">
-                <input type="text" id="attack" value="{{old('attack', $character->attack)}}" name="attack" placeholder="Attack" class="form-control my-2" required>
-                <input type="text" id="defence" value="{{old('defence', $character->defence)}}" name="sale_date" placeholder="Sale" class="form-control my-2">
-                <input type="text" id="life" name="life" value="{{old('life', $character->life)}}" placeholder="HP" class="form-control my-2" required>
-                <input type="text" id="speed" name="speed" value="{{old('speed', $character->speed)}}" placeholder="Speed" class="form-control my-2" required>
-                <input type="url" id="thumb" name="thumb" placeholder="add url image" class="form-control">
-                <button type="submit" class="btn btn-primary">Save</button>
-        </form>
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Profile') }}
-    </h2>
-    <div class="card p-4 mb-4 bg-white shadow rounded-lg">
-
-        @include('profile.partials.update-profile-information-form')
-
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
     </div>
-
-    <div class="card p-4 mb-4 bg-white shadow rounded-lg">
-
-
-        @include('profile.partials.update-password-form')
-
-    </div>
-
-    <div class="card p-4 mb-4 bg-white shadow rounded-lg">
-
-
-        @include('profile.partials.delete-user-form')
-
-    </div>
-</div>
-
+    @endif
+    <form action="{{route('character.update', $character->id)}}" method="POST" class="p-3">
+        @csrf
+        @method('PUT')
+            <input type="text" id="name" value="{{old('name', $character->name)}}" name="name" placeholder="Name" class="form-control my-2" required>
+            <input type="text" id="description" value="{{old('description', $character->description)}}" name="Description" placeholder="Description" class="form-control my-2">
+            <input type="text" id="attack" value="{{old('attack', $character->attack)}}" name="attack" placeholder="Attack" class="form-control my-2" required>
+            <input type="text" id="defence" value="{{old('defence', $character->defence)}}" name="sale_date" placeholder="Sale" class="form-control my-2">
+            <input type="text" id="life" name="life" value="{{old('life', $character->life)}}" placeholder="HP" class="form-control my-2" required>
+            <input type="text" id="speed" name="speed" value="{{old('speed', $character->speed)}}" placeholder="Speed" class="form-control my-2" required>
+            <input type="url" id="thumb" name="thumb" placeholder="add url image" class="form-control">
+            <button type="submit" class="btn btn-primary">Save</button>
+    </form>
 @endsection
