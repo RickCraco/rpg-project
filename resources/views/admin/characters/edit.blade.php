2bf71f7911600b558d23e06df6ab0658128a3bed
federicomanni97 <manni_federico@libero.it>
2024-01-15 17:23:57 +0100

    input type file

--- resources/views/admin/characters/edit.blade.php
+++ resources/views/admin/characters/edit.blade.php
@@ -11,7 +11,7 @@
             </ul>
         </div>
         @endif
-        <form action="{{route('character.update', $character->id)}}" method="POST" class="p-3">
+        <form action="{{route('character.update', $character->id)}}" enctype="multipart/form-data" method="POST" class="p-3">
             @csrf
             @method('PUT')
                 <input type="text" id="name" value="{{old('name', $character->name)}}" name="name" placeholder="Name" class="form-control my-2" required>
@@ -21,6 +21,7 @@
                 <input type="text" id="life" name="life" value="{{old('life', $character->life)}}" placeholder="HP" class="form-control my-2" required>
                 <input type="text" id="speed" name="speed" value="{{old('speed', $comic->speed)}}" placeholder="Speed" class="form-control my-2" required>
                 <input type="url" id="thumb" name="thumb" placeholder="add url image" class="form-control">
+                <input type="file" id="image" name="image" value="{{old('image', $character->image)}}" class="form-control my-2">
                 <button type="submit" class="btn btn-primary">Save</button>
         </form>
     <h2 class="fs-4 text-secondary my-4">
