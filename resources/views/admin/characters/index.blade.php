d4b5f37f145212bcef570fa69c6a194c9e7ed50d
alexraduca01 <alex.raduca01@icloud.com>
2024-01-15 20:47:40 +0100

    fixed index table style

--- resources/views/admin/characters/index.blade.php
+++ resources/views/admin/characters/index.blade.php
@@ -8,21 +8,21 @@
                     <table class="table table-striped">
                         <thead>
                             <tr>
-                                <th scope="col">ID</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Created at</th>
-                                <th scope="col">Last update</th>
-                                <th scope="col">Settings</th>
+                                <th scope="col" class="text-center">ID</th>
+                                <th scope="col" class="text-center">Name</th>
+                                <th scope="col" class="text-center">Created at</th>
+                                <th scope="col" class="text-center">Last update</th>
+                                <th scope="col" class="text-center">Settings</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($characters as $item)
                                 <tr>
-                                    <th>{{ $item->id }}</th>
-                                    <td><a class="text-decoration-none text-black" href="{{ route('admin.characters.show', $item->id) }}">{{ $item->name }}</a></td>
-                                    <td>{{ $item->created_at }}</td>
-                                    <td>{{ $item->updated_at }}</td>
-                                    <td>
+                                    <td class="text-center">{{ $item->id }}</td>
+                                    <td class="text-center"><a class="text-decoration-none text-black" href="{{ route('admin.characters.show', $item->id) }}">{{ $item->name }}</a></td>
+                                    <td class="text-center">{{ $item->created_at }}</td>
+                                    <td class="text-center">{{ $item->updated_at }}</td>
+                                    <td class="text-center">
                                         <a href="{{ route('admin.characters.edit', $item->id) }}" class="btn btn-success"><i class="fa-solid fa-gear"></i></a>
                                         <a href="{{ route('admin.characters.destroy', $item->id) }}" class="btn btn-danger"><i class="fa-solid fa-trash"></i></a>
                                     </td>
