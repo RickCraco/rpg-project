4ca413badb0079360693b2c0b31312beb440d157
federicomanni97 <manni_federico@libero.it>
2024-01-15 17:53:12 +0100

    edit fixed

--- resources/views/admin/characters/edit.blade.php
+++ resources/views/admin/characters/edit.blade.php
@@ -24,28 +24,5 @@
                 <input type="file" id="image" name="image" value="{{old('image', $character->image)}}" class="form-control my-2">
                 <button type="submit" class="btn btn-primary">Save</button>
         </form>
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Profile') }}
-    </h2>
-    <div class="card p-4 mb-4 bg-white shadow rounded-lg">
-
-        @include('profile.partials.update-profile-information-form')
-
-    </div>
-
-    <div class="card p-4 mb-4 bg-white shadow rounded-lg">
-
-
-        @include('profile.partials.update-password-form')
-
-    </div>
-
-    <div class="card p-4 mb-4 bg-white shadow rounded-lg">
-
-
-        @include('profile.partials.delete-user-form')
-
-    </div>
 </div>
-
 @endsection
