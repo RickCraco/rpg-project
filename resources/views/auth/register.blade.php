dd0cd61ee7c8fbe3b4197741a9169b1b292863df
alexraduca01 <alex.raduca01@icloud.com>
2024-01-15 16:04:36 +0100

    updated register page style

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,9 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header bg-dark text-white">{{ __('Register') }}</div>
 
-                <div class="card-body">
+                <div class="card-body bg-dark-subtle">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
@@ -63,7 +63,7 @@
 
                         <div class="mb-4 row">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-success">
                                     {{ __('Register') }}
                                 </button>
                             </div>
