14f9c3f334d66f66b457c8be3a4a7a682665d4be
DavideGila <davide.gila@gmail.com>
2024-01-18 17:07:27 +0100

    added a foreach to show all the characters with that specif type

--- resources/views/admin/types/show.blade.php
+++ resources/views/admin/types/show.blade.php
@@ -1,18 +1,23 @@
 @extends('layouts.app')
 @section('content')
-<main class="bg-dark">
-    <section class="container py-4">
-        <h1 class="text-white">{{$type->name}}</h1>
-        <div class="row gy-4">
-            <div class="col-12 w-50">
-                <div class="card">
-                    <div class="card-body">
-                        <p class="card-text">{{$type->description}}</p>
+    <main class="bg-dark">
+        <section class="container py-4">
+            <h1 class="text-white">{{ $type->name }}</h1>
+            <div class="row gy-4">
+                <div class="col-12 w-50">
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">{{ $type->description }}</p>
+                            <h4>Charaters with this Type</h4>
+                            @foreach ($type->characters as $type)
+                                <li><a class="text-dark text-decoration-none"
+                                        href="{{ route('admin.characters.show', $type->id) }}">{{ $type->name }}</a></li>
+                            @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <a href="{{route('admin.types.edit', $type)}}" class="text-white btn btn-primary my-3">Edit</a>
-    </section>
-</main>
+            <a href="{{ route('admin.types.edit', $type) }}" class="text-white btn btn-primary my-3">Edit</a>
+        </section>
+    </main>
 @endsection
