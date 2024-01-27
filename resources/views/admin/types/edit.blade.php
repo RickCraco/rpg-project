a300279469b06f4a3d8c7d65a13b1996246ebd72
RickCraco <riccardocracolici43@gmail.com>
2024-01-27 18:47:13 +0100

    added labels for img and icon

--- resources/views/admin/types/edit.blade.php
+++ resources/views/admin/types/edit.blade.php
@@ -16,7 +16,9 @@
             @method('PUT')
                 <input type="text" id="name" value="{{old('name', $type->name)}}" name="name" placeholder="Name" class="form-control my-2">
                 <input type="text" id="description" value="{{old('description', $type->description)}}" name="description" placeholder="Description" class="form-control my-2">
+                <label for="image" class="text-white">Image</label>
                 <input type="file" id="image" name="image" value="{{old('image', $type->image)}}" class="form-control my-2">
+                <label for="icon" class="text-white">Icon</label>
                 <input type="file" id="icon" name="icon" value="{{old('icon', $type->icon)}}" class="form-control my-2">
                 <button type="submit" class="btn btn-primary">Save</button>
         </form>
